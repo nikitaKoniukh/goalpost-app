a7e890f54dd611ad98dca33b4912cbbca74cb758
Nikita Koniukh <koniukh.nikita@gmail.com>
2019-04-25 06:50:36 +0300

    Initial Commit

---
+++ goalpost-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Nikita Koniukh on 25/04/2019.
+//  Copyright © 2019 Nikita Koniukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

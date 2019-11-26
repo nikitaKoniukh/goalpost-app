eaa67681a04e6c4377678464041c6889444a264c
Nikita Koniukh <44729824+nikitaKoniukh@users.noreply.github.com>
2019-11-26 19:17:26 +0200

    Revert "Revert "UI upgrade""
    This reverts commit e7baa860a1b26a3d0669825def0395cb8128b400.

--- goalpost-app/Extensions/UIExt.swift
+++ goalpost-app/Extensions/UIExt.swift
@@ -10,9 +10,9 @@ import UIKit
 
 extension UIButton{
     func setSelectedColor(){
-        self.backgroundColor = #colorLiteral(red: 0.4274509804, green: 0.737254902, blue: 0.3882352941, alpha: 1)
+        self.backgroundColor = #colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1)
     }
     func deSelectedColor(){
-        self.backgroundColor = #colorLiteral(red: 0.6980392157, green: 0.8666666667, blue: 0.6862745098, alpha: 1)
+        self.backgroundColor = #colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 0.5)
     }
 }

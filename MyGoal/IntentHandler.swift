424dc72a925d5fbd6d36ed6938923dd9cbc0eab4
Nikita Koniukh <44729824+nikitaKoniukh@users.noreply.github.com>
2019-11-27 15:04:48 +0200

    Update IntentHandler.swift

--- MyGoal/IntentHandler.swift
+++ MyGoal/IntentHandler.swift
@@ -9,12 +9,12 @@
 import Intents
 
 class IntentHandler: INExtension {
-    
+
     override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation.  If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent.
-        
-        return self
+                guard intent is NewGoalIntent else {
+                    fatalError("Unhandled Intent error : \(intent)")
+                }
+        return MyGoalIntentHandler()
     }
-    
+
 }
